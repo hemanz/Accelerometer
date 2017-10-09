dcefe9f69d7a274269ec74e8a14787604ee6ea5e
zhanghenan <zhanghenan@sensetime.com>
2017-10-09 16:58:27 +0800

    init the project

---
+++ Accelerometer/Accelerometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accelerometer
+//
+//  Created by zhanghenan on 2017/10/9.
+//  Copyright © 2017年 HN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
